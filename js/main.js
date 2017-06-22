0fc5e62926ae0dbd5be9f6e202bbf90fc0d4818e
Johnathan Howell <me@johnathanhowell.com>
2017-06-21 22:00:27 -0400

    update timer dynamically, set release date

--- js/main.js
+++ js/main.js
@@ -3,18 +3,23 @@ const minute = second * 60
 const hour = minute * 60
 const day = hour * 24
 
-const releaseDate = new Date(2017, 5, 29)
-const timeLeft = releaseDate - new Date()
+const releaseDate = new Date(1498658400*1000)
 
-const daysLeft = Math.floor(timeLeft / day).toString()
-const hoursLeft = Math.floor((timeLeft % day) / hour).toString()
-const minutesLeft = Math.floor((timeLeft % hour) / minute).toString()
+function updateTimer() {
+	const timeLeft = releaseDate - new Date()
+	const daysLeft = Math.floor(timeLeft / day).toString()
+	const hoursLeft = Math.floor((timeLeft % day) / hour).toString()
+	const minutesLeft = Math.floor((timeLeft % hour) / minute).toString()
 
-document.getElementById('daysleft-tens').innerHTML = Math.floor(daysLeft / 10 % 10)
-document.getElementById('daysleft').innerHTML = daysLeft % 10
-document.getElementById('hoursleft-tens').innerHTML = Math.floor(hoursLeft / 10 % 10)
-document.getElementById('hoursleft-ones').innerHTML = hoursLeft % 10
-document.getElementById('minutesleft-tens').innerHTML = Math.floor(minutesLeft / 10 % 10)
-document.getElementById('minutesleft-ones').innerHTML = minutesLeft % 10
+	document.getElementById('daysleft-tens').innerHTML = Math.floor(daysLeft / 10 % 10)
+	document.getElementById('daysleft').innerHTML = daysLeft % 10
+	document.getElementById('hoursleft-tens').innerHTML = Math.floor(hoursLeft / 10 % 10)
+	document.getElementById('hoursleft-ones').innerHTML = hoursLeft % 10
+	document.getElementById('minutesleft-tens').innerHTML = Math.floor(minutesLeft / 10 % 10)
+	document.getElementById('minutesleft-ones').innerHTML = minutesLeft % 10
+}
+
+updateTimer()
+setInterval(updateTimer, minute)
 
 
