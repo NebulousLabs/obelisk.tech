1bc6e0c060d520b27a7ed9f94ceb61cef36b41fd
Johnathan Howell <me@johnathanhowell.com>
2017-07-05 20:05:04 -0400

    update nsold, add link to faq

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ function updateNav() {
 window.onscroll = updateNav
 updateNav()
 
-const nSold = 800
+const nSold = 1030
 const sellCap = 4000
 $('.order-bar-inner').css('width', (nSold/sellCap) * 100 + '%')
 $('#sold').text(nSold)
