eeaa6a9d03c9f35be01f2863ebd5fafa91d22497
Johnathan Howell <me@johnathanhowell.com>
2017-07-10 16:30:53 -0400

    fixup shadowed js declaration

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ const expectedHashrate = 100 // GH/s
 const expectedPowerConsumption = 500 // W
 const blockReward = 20000000 // SC per day on end of june 2018
 let obelisks = 1
-let sold = 4000
+const sellCap = 4000
+const nSold = 1240
 let electricityprice = 0.10
 
 // monthlyRevenue returns the estimated monthly revenue given the number of
 // obelisks and the number of obelisks sold
-const monthlyRevenue = (obelisks, sold) => obelisks / sold * blockReward * 30
+const monthlyRevenue = (obelisks) => obelisks / sellCap * blockReward * 30
 
 // monthlyElectricityCost returns the estimated monthly electricty cost given
 // the cost of electricity per KW/H
 const monthlyElectricityCost = (obelisks, electricityprice) => (obelisks*electricityprice) * (500 / 1000) * 24 * 30
 const updateCosts = () => {
 	$('#electricity-cost-result').text(Math.round(monthlyElectricityCost(obelisks, electricityprice)))
-	$('#mining-reward-result').text(Math.round(monthlyRevenue(obelisks, sold)).toLocaleString())
+	$('#mining-reward-result').text(Math.round(monthlyRevenue(obelisks)).toLocaleString())
 }
 
 $('#quantity-input').on('input', (e) => {
@@ -22,13 +23,6 @@ $('#quantity-input').on('input', (e) => {
 		return
 	}
 	obelisks = parseInt(e.target.value, 10)
-	updateCosts()
-})
-$('#nsold-input').on('input', (e) => {
-	if (isNaN(parseInt(e.target.value, 10))) {
-		return
-	}
-	sold = parseInt(e.target.value, 10)
 	updateCosts()
 })
 $('#electricity-cost').on('input', (e) => {
@@ -62,8 +56,6 @@ function updateNav() {
 window.onscroll = updateNav
 updateNav()
 
-const nSold = 1240
-const sellCap = 4000
 $('.order-bar-inner').css('width', (nSold/sellCap) * 100 + '%')
 $('#sold').text(nSold)
 $('#sellcap').text(sellCap)
