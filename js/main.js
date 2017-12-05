74abd0342acc78e6a4f5d39dc6419650ba15f50a
Zach Herbert <zachherbert@users.noreply.github.com>
2017-12-05 13:49:32 -0500

    Updated order counts

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ const dcr1BlockReward = 3570 // DCR per day on end of june 2018
 let obelisks = 1
 let electricityPrice = 0.1
 
-const sc1Sold = 3479
+const sc1Sold = 3514
 const sc1SellCap = 4000
 
-const dcr1Sold = 2763
+const dcr1Sold = 2884
 const dcr1SellCap = 4000
 
 // monthlyRevenue returns the estimated monthly revenue given the number of
