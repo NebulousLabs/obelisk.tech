a33a155a714f3e2f2cac182fe4a057b494bdecad
Ken Carpenter <ken@mindjuice.net>
2018-03-22 17:55:55 -0400

    Rename preorder folders to presale to avoid caching problems
    This is a temporary measure until we can figure out why things are being cached despite adding several "anti-caching" headers and removing the React service worker

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -e
 
 rm -rf release/
-cd preorder-sc1
+cd presale-sc1
 npm run build
 cd ../
-cd preorder-dcr1
+cd presale-dcr1
 npm run build
 cd ../
 mkdir release/
@@ -14,6 +14,6 @@ cp about.html release/
 cp colocation.html release/
 cp preorder.html release/
 cp -r assets release/
-cp -r preorder-sc1/build release/preorder-sc1
-cp -r preorder-dcr1/build release/preorder-dcr1
+cp -r presale-sc1/build release/presale-sc1
+cp -r presale-dcr1/build release/presale-dcr1
 cp -r js release/
--- presale-dcr1/src/index.js
+++ presale-dcr1/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 // import registerServiceWorker from './registerServiceWorker';
 
+import { unregister } from './registerServiceWorker'
+
 ReactDOM.render(<App />, document.getElementById('root'))
 // registerServiceWorker();
+
+unregister()
